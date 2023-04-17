67efba65f6653fb13635520484e59af545f8deee
Jonathas <jonathasdasilvacordeiro@gmail.com>
2023-04-16 23:11:13 -0300

    exercise finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if(typeof start === "string" || typeof end === "string"){
+        return "ERROR"
+    }
+    if(start >= 0 && end >= 0){
+        let sub = Math.abs(start - end)
+        let total = sub+1
+        for(let i = 0;i <= sub;i++){
+            total += i
+        }
+        return Math.abs(total)
+        }
+    else{return 'ERROR'
+    }
+
+
 
 };
 
